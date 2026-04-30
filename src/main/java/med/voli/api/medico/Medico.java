1e22d12fda3409cb85fde53057da4d76c927bff0
Ricardo Carvalho <ricardo-svc@live.com>
2026-04-30 20:14:55 -0300

    Criando as Entidades

---
+++ src/main/java/med/voli/api/medico/Medico.java
@@ -0,0 +1,28 @@
+package med.voli.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voli.api.DTO.DadosEndereco;
+import med.voli.api.DTO.Especialidade;
+import med.voli.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
